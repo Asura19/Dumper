0621f2af834e4395d59055d3de3bc2e031b9ddab
Phoenix <x.rhythm@qq.com>
2025-11-10 14:41:42 +0800

    SpringBoardUIServices apis

--- Dumper/Dumper/ViewController.m
+++ Dumper/Dumper/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [FrameworkDumper dumpFrameworkAtPath:@"/System/Library/PrivateFrameworks/SpringBoardServices.framework"];
+    [FrameworkDumper dumpFrameworkAtPath:@"/System/Library/PrivateFrameworks/SpringBoardUIServices.framework"];
 }
 
 
